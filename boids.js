b9f859404a2b04272113e037b250bd8efa281b00
no programmer <maiello.frank@gmail.com>
2026-01-24 23:50:21 -0500

    Add files via upload

--- boids.js
+++ boids.js
@@ -1,5 +1,5 @@
 /*
-B0IDS 1.54 :: emergent flocking behavior ::
+B0IDS 1.55 :: emergent flocking behavior ::
 copyright 2026 :: Frank Maiello :: maiello.frank@gmail.com ::
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -7244,7 +7244,7 @@ function drawSimMenu() {
     c.textBaseline = 'bottom';
     c.font = `${0.24 * knobRadius}px monospace`;
     c.fillStyle = `hsla(0, 0%, 60%, ${menuOpacity})`;
-    c.fillText('v1.54', padding + 4.8 * knobRadius, menuHeight + padding - 0.15 * knobRadius);
+    c.fillText('v1.55', padding + 4.8 * knobRadius, menuHeight + padding - 0.15 * knobRadius);
 
     c.restore();
 }
