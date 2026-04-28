e9e8eb779e398e0eb58888a3cfd5304c2ec31cee
no programmer <maiello.frank@gmail.com>
2026-04-28 00:46:12 -0400

    Add files via upload

--- Fireworks.js
+++ Fireworks.js
@@ -1,6 +1,11 @@
 // Fireworks 3D
 // copyright 2026 :: Frank Maiello :: maiello.frank@gmail.com
 
+// "Those who would give up essential Liberty, 
+// to purchase a little temporary Safety, 
+// deserve neither Liberty nor Safety."
+// - Benjamin Franklin
+
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
